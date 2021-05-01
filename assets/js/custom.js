397354a67c11c0cff975c2bf02c809a0816c3bcf
Rayhan <ridoykhan.cse.web@gmail.com>
2021-05-01 19:21:53 +0600

    customers and carousel area here

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,32 @@
+// -----------> javaScript
+// -----------------------
+// header area ends here
+// ------------------------ 
 const headerMenu = document.querySelector(".header");
 const navToggle = document.querySelector(".nav-toggler");
 const navigation = document.querySelector("ul.nav");
-
 navToggle.onclick = ()=>{
     navigation.classList.toggle("active");
     navToggle.classList.toggle("active");
 }
-
 window.onscroll = ()=>{
     this.scrollY > 20 ?
     headerMenu.classList.add("active"):
     headerMenu.classList.remove("active");
 }
+
+// -----------> Jquery
+// -----------------------
+// customers carousel area here
+// ------------------------ 
+// owl carousel 
+$(document).ready(function(){
+    $('.customers-carousel-items').owlCarousel({
+        loop:true,
+        nav:false,
+        items:1,
+        autoplay:true,
+        autoplayTimeout: 5000,
+        smartSpeed: 2000
+    });
+  });
