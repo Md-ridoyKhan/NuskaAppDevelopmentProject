f268259150e5e0c2a21bd9218c2aaa1483678838
Rayhan <ridoykhan.cse.web@gmail.com>
2021-05-06 20:55:47 +0600

    topUp scroll button add here

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,57 @@
 // -----------> javaScript
 
 // -----------------------
-// preloader area here 
-// ------------------------ 
-// jquery 
-$(window).on("load", function(){
-    $(".preloder").fadeOut(1000);
-});
 // -----------------------
-// preloader area ends here 
 // ------------------------ 
+// // -----------> Jquery
+$(document).ready(function(){
+
+    // -----------------------
+    // topUp-btn area here
+    // ------------------------ 
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 80){
+            $(".topUp-btn").fadeIn(800);
+        } else{
+            $(".topUp-btn").fadeOut(800);
+        }
+    });
+    $(".topUp-btn").click(function(){
+        $(" html,body").animate({scrollTop : 0}, 1000);
+    });
+
+    // -----------------------
+    // preloader area here 
+    // ------------------------ 
+    $(window).on("load", function(){
+        $(".preloder").fadeOut(1000);
+    });
+
+    // -----------------------
+    // customers carousel area here
+    // ------------------------ 
+    $('.customers-carousel-items').owlCarousel({
+        loop:true,
+        nav:false,
+        items:1,
+        autoplay:true,
+        autoplayTimeout: 5000,
+        smartSpeed: 2000
+    });
+
+    // -----------------------
+    // This will create a video gallery magnific popup
+    // -----------------------
+    $('.expand-video').magnificPopup({
+        type: 'iframe'
+    });
+
+});
 
 // -----------------------
 // header area here
 // ------------------------ 
 const headerMenu = document.querySelector(".header");
-const www = document.querySelector(".header-content .nav > li > a");
 const navToggle = document.querySelector(".nav-toggler");
 const navigation = document.querySelector("ul.nav");
 navToggle.onclick = ()=>{
@@ -28,24 +64,3 @@ window.onscroll = ()=>{
     headerMenu.classList.remove("active");
 }
 
-// -----------> Jquery
-// -----------------------
-// customers carousel area here
-// ------------------------ 
-// owl carousel 
-$(document).ready(function(){
-    $('.customers-carousel-items').owlCarousel({
-        loop:true,
-        nav:false,
-        items:1,
-        autoplay:true,
-        autoplayTimeout: 5000,
-        smartSpeed: 2000
-    });
-});
-// -----------------------
-// This will create a video gallery magnific popup
-// -----------------------
-$('.expand-video').magnificPopup({
-    type: 'iframe'
-});
